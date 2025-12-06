0f3aa2045080728e38a5001610d1c0306a45eac8
Athan <jonpuray@gmail.com>
2025-12-06 18:15:37 +0800

    1st commit

--- db.py
+++ db.py
@@ -4,6 +4,7 @@ import streamlit as st
 
 _mongo_client = None
 
+@st.cache_resource
 def get_client():
     global _mongo_client
     if _mongo_client is None:
@@ -11,12 +12,14 @@ def get_client():
         _mongo_client = MongoClient(uri, serverSelectionTimeoutMS=5000)
     return _mongo_client
 
+@st.cache_resource
 def get_collection(collection_name: str):
     client = get_client()
     db_name = os.environ.get('MMI_DBusers', 'mats')
     db = client[db_name]
     return db[collection_name]
 
+@st.cache_resource
 def get_leave_types():
     client = get_client()
     db_name = os.environ.get('MMI_DBusers', 'mats')
