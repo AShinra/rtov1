5943a67951351598166a5e3763d8dfbdb6b13741
Athan <jonpuray@gmail.com>
2026-08-27 11:35:49 +0800

    update

--- admin_tools.py
+++ admin_tools.py
@@ -17,68 +17,68 @@ def user_management():
         tab1, tab2, tab3 = st.tabs(["✍️**Add User**", "🔎**View Users**", "🪪Leave Credits"])
 
         
-        # with tab1:
-        #     st.subheader("✍️Add User")
-        #     gradient_line()
+        with tab1:
+            st.subheader("✍️Add User")
+            gradient_line()
 
-        #     cola, colb = st.columns(2, border=True)
-        #     with cola:
-        #         st.subheader("User Info")
-        #         gradient_line()
-        #         st.text_input(
-        #             label="**Username**",
-        #             key='add_user_name_input',
-        #             placeholder='Enter Username')
+            cola, colb = st.columns(2, border=True)
+            with cola:
+                st.subheader("User Info")
+                gradient_line()
+                st.text_input(
+                    label="**Username**",
+                    key='add_user_name_input',
+                    placeholder='Enter Username')
 
-        #         st.text_input(
-        #             label="**Password**",
-        #             type="password",
-        #             key='add_user_password_input',
-        #             placeholder='Enter Password')
+                st.text_input(
+                    label="**Password**",
+                    type="password",
+                    key='add_user_password_input',
+                    placeholder='Enter Password')
                 
-        #         st.text_input(
-        #             label="**Full Name**",
-        #             key='add_user_fname_input',
-        #             placeholder='Enter Full Name')
+                st.text_input(
+                    label="**Full Name**",
+                    key='add_user_fname_input',
+                    placeholder='Enter Full Name')
                 
-        #         st.selectbox(
-        #             label="**Permission**",
-        #             options=['user', 'admin'],
-        #             placeholder='Select Permission',
-        #             key='add_user_permission_selectbox',
-        #             index=None)
-        #     with colb:
-        #         departments = ['Operations']
-        #         teams = ['Broadcast', 'Online', 'Print', 'Provincial']
-        #         roles = ['Lead', 'Lead-Assistant', 'Member']
+                st.selectbox(
+                    label="**Permission**",
+                    options=['user', 'admin'],
+                    placeholder='Select Permission',
+                    key='add_user_permission_selectbox',
+                    index=None)
+            with colb:
+                departments = ['Operations']
+                teams = ['Broadcast', 'Online', 'Print', 'Provincial']
+                roles = ['Lead', 'Lead-Assistant', 'Member']
                 
-        #         st.subheader("Role Info")
-        #         gradient_line()
-        #         st.selectbox(
-        #             label="**Department**",
-        #             options=departments,
-        #             placeholder='Select Department',
-        #             key='add_user_department_selectbox',
-        #             index=None)
+                st.subheader("Role Info")
+                gradient_line()
+                st.selectbox(
+                    label="**Department**",
+                    options=departments,
+                    placeholder='Select Department',
+                    key='add_user_department_selectbox',
+                    index=None)
 
-        #         st.selectbox(
-        #             label="**Team**",
-        #             options=teams,
-        #             placeholder='Select Team',
-        #             key='add_user_team_selectbox',
-        #             index=None)
+                st.selectbox(
+                    label="**Team**",
+                    options=teams,
+                    placeholder='Select Team',
+                    key='add_user_team_selectbox',
+                    index=None)
                 
-        #         st.selectbox(
-        #             label="**Team-Role**",
-        #             options=roles,
-        #             placeholder='Select Team-Role',
-        #             key='add_user_role_selectbox',
-        #             index=None)
+                st.selectbox(
+                    label="**Team-Role**",
+                    options=roles,
+                    placeholder='Select Team-Role',
+                    key='add_user_role_selectbox',
+                    index=None)
                 
-        #     st.button(
-        #         label="Add User",
-        #         width='stretch',
-        #         key='add_user_button')
+            st.button(
+                label="Add User",
+                width='stretch',
+                key='add_user_button')
             
 
         with tab2:
@@ -279,12 +279,12 @@ def user_management():
     #     # create leave credit document
     #     leave_credits_collection = get_collection('leave_credits')
     #     leave_credits_doc = {
-    #         'Sick':10,
-    #         'Vacation':10,
+    #         'Sick':0,
+    #         'Vacation':0,
     #         'Others':0,
-    #         'Birthday':1,
-    #         'Bereavement':3,
-    #         'Emergency':2,
+    #         'Birthday':0,
+    #         'Bereavement':0,
+    #         'Emergency':0,
     #         'Maternity':0,
     #         'Paternity':0}
         
