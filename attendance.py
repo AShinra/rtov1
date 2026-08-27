97693a1fc73cdc7c99e9044be5ad6ac41b2cee4f
Athan <jonpuray@gmail.com>
2026-08-27 11:55:06 +0800

    update

--- attendance.py
+++ attendance.py
@@ -287,7 +287,9 @@ def user_info(fname: str, rights: str):
                         with col1:
                             # create a selection box for team members
                             users = set(df_users_team['Name'].to_list())
-                            users = sorted(users)
+                            # users = sorted(users)
+
+                            st.write(sorted(users))
                             select_name = st.selectbox(
                                 label='Select Team Member',
                                 options=users,
